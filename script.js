e6646165c554c90d18d3595a8d390b2c63a4dad2
DeYana Simmons-Howard <deyana.howard@gmail.com>
2021-12-13 12:33:28 -0500

    added questions

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -13,5 +15,31 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var password = '';
+
+  var pwdLength = prompt("Between 8 and 128 how long would you like your password to be?");
+
+  while (pwdLength < 8 || pwdLength > 128) {
+    alert('Your password must be a number between 8 and 128 characters only.');
+    pwdLength = prompt("Between 8 and 128 how long would you like your password to be?");
+  };
+
+  var wantsUpper = confirm('Would you like uppercase letters?');
+  var wantsLower = confirm('Would you like lowercase letters?');
+  var wantsNumbers = confirm('WOuld you like numbers?');
+  var wantsSpecial = confirm('Would you like special characters?');
+
+  while (!wantsUpper && !wantsLower && !wantsNumbers && !wantsSpecial) {
+    alert('You must choose at least one character type.');
+    wantsUpper = confirm('Would you like uppercase letters?');
+    wantsLower = confirm('Would you like lowercase letters?');
+    wantsNumbers = confirm('WOuld you like numbers?');
+    wantsSpecial = confirm('Would you like special characters?');
+  }
+
+  return password;
+}
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
