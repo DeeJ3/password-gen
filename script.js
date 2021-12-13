d30d4512f586d4733e0fd6195a980e53fcf8e082
DeYana Simmons-Howard <deyana.howard@gmail.com>
2021-12-13 12:51:41 -0500

    created conditionals

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Assignment code here
-
+var uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowers = 'abcdefghijklmnopqrstuvwxyz';
+var numbers = '1234567890';
+var special = '!@#$%^&*()_+=-'
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,31 +18,59 @@ function writePassword() {
 
 }
 
+// returning a random password
 function generatePassword() {
+  // 
   var password = '';
+  var characters = '';
 
+  // 
   var pwdLength = prompt("Between 8 and 128 how long would you like your password to be?");
-
+ 
+  // 
   while (pwdLength < 8 || pwdLength > 128) {
     alert('Your password must be a number between 8 and 128 characters only.');
     pwdLength = prompt("Between 8 and 128 how long would you like your password to be?");
   };
 
+  // 
   var wantsUpper = confirm('Would you like uppercase letters?');
   var wantsLower = confirm('Would you like lowercase letters?');
   var wantsNumbers = confirm('WOuld you like numbers?');
   var wantsSpecial = confirm('Would you like special characters?');
 
+  // 
   while (!wantsUpper && !wantsLower && !wantsNumbers && !wantsSpecial) {
     alert('You must choose at least one character type.');
     wantsUpper = confirm('Would you like uppercase letters?');
     wantsLower = confirm('Would you like lowercase letters?');
     wantsNumbers = confirm('WOuld you like numbers?');
     wantsSpecial = confirm('Would you like special characters?');
+  };
+
+  // 
+  if (wantsUpper) {
+    characters += uppers
+  };
+  if (wantsLower) {
+    characters += lowers
+  };
+  if (wantsNumbers) {
+    characters += numbers
+  };
+  if (wantsSpecial) {
+    characters += special
+  };
+
+  console.log('characters', characters);
+
+  // 
+  for (var i = 0; i < pwdLength; i++) {
+    password += characters.charAt(Math.floor(Math.random() * characters.length));
   }
 
   return password;
-}
+};
 
 
 // Add event listener to generate button
